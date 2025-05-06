da6f5e0031d2f4617501bba41868ba7fc7e58cd5
Arielle-CodeStudio <arielle.codestudio@gmail.com>
2025-05-06 14:20:40 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function runTypingEffect() {
+  const text = 'Welcome to Design Studios.'
+  const runTypingElement = document.getElementById('typing-text');
+  const typingDelay = 100;
+
+  typeText(text, runTypingElement, typingDelay);
+}
+
+function typeText(text, runTypingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => { 
+      runTypingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
